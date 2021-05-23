fa334d4a5d02c56a4577a1b60e64316cdd92c6da
baboomerang <irodrigue101@gmail.com>
2021-05-22 21:36:01 -0400

    Working s_print function, hello world prints to bootloader

--- bootloader.asm
+++ bootloader.asm
@@ -1,25 +1,31 @@
-section .data
-    msg    db "Hello World!", 0x0
-    len    equ $ - msg
+[org 0x7c00]
 
 section .text
     global _start
 
 _start:
-    xor    ax, ax
-    mov    ah, 0x0e
-
-    lea    si, [msg]
-.print:
-    lodsb
-    jz .end
-    int 0x10
-    jmp .print
-.end:
-
+    mov    si, msg
+    call   s_print
+_end:
     jmp    $
-    times  510 - ($ - $$) db 0
 
+;------------------------
+;  s_print(char* string)
+;  DS:(E)SI - char*
+;  Returns: void
+;  Clobbers: AH, AL
+;------------------------
+s_print:
+    mov    ah, 0xe
+.L1:
+    lodsb
+    test   al, al
+    jz     .L2
+    int    0x10
+    jmp    .L1
+.L2:
+    ret
 
-
+msg  db "Hello World!", 0x0
+times  510 - ($ - $$) db 0x0
 dw  0xaa55
